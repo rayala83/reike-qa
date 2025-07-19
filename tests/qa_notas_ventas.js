8c3dbb976648726700a1421b50b91fe9bb6fedb9
rodrigo.ayala <ragieci@gmail.com>
2025-07-19 10:25:45 -0400

    se agrega boton de enviar producto para finalizar

--- tests/qa_notas_ventas.js
+++ tests/qa_notas_ventas.js
@@ -1,4 +1,5 @@
 const {firefox} = require ("playwright");
+const assert = require('assert');
 
 (async () => {
 
@@ -18,15 +19,29 @@ const {firefox} = require ("playwright");
     await page.selectOption('#sales_note_type_document_sii', '39');
     await page.selectOption('#sales_note_ware_house_id', '13');
 
-    await page.click('#select2-sales_note_customer_id-container');
-    await page.fill('.select2-search__field', 'fa');
-    await page.click('.select2-results__option >> text=falabella');
+    // await page.click('#select2-sales_note_customer_id-container');
+    // await page.fill('.select2-search__field', 'gi');
+    // await page.click('.select2-results__option >> text=Gitz SpA');
+    
+
 
     await page.selectOption('#sales_note_currency', 'pesos');    
 
     await page.click('#select2-sales_note_e_document_products_attributes_0_product_id-container');
     await page.fill('.select2-search__field', 'cama');
     await page.click('.select2-results__option', { hasText: 'Cama matrimonial' });
+
+    await page.fill('#sales_note_e_document_products_attributes_0_quantity', '1');
+
+    await page.waitForFunction(() => {
+    const input = document.querySelector('#sales_note_e_document_products_attributes_0_price');
+    return input && input.value.trim() !== "";
+    });
+
+    page.once('dialog', async dialog => {
+    await dialog.accept();
+    });
+    await page.click('button:has-text("Enviar")');
 
     await page.pause()
     await browser.close()
